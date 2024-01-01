35b971087723a2862a940f6edaa8df2c471c078e
Devin <dbelle722@gmail.com>
2024-01-01 12:19:49 -0500

    Starting Files

---
+++ script.js
@@ -0,0 +1,5 @@
+//Need to create globally defined variables
+//Create a fetch call to pull city user search with latitude and longitude
+//Create a second fetch call that links the lat and lon value with city name to get forecast details
+//create a function for getting user-search input out of local storage
+//Create a function for saving user search input into local storage
